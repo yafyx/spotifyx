a59b632d2c47854bd4e5618c140d371c9c3afdf5
fyx <info@yfyx.me>
2025-03-20 06:25:36 +0700

    Remove page transition styles

--- src/theme.js
+++ src/theme.js
@@ -16,22 +16,4 @@
   } else {
     document.body.classList.add("control-nav");
   }
-
-  // Add subtler page transitions
-  const applyPageTransitions = () => {
-    const style = document.createElement("style");
-    style.innerHTML = `
-      .main-view-container__scroll-node-child {
-        animation: fadeIn 0.25s ease;
-      }
-      
-      @keyframes fadeIn {
-        from { opacity: 0.8; }
-        to { opacity: 1; }
-      }
-    `;
-    document.head.appendChild(style);
-  };
-
-  applyPageTransitions();
 })();
